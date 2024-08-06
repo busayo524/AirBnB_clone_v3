707289c884d160ffd5b839de435a4b374ebe4c48
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-08-06 05:14:19 +0100

    error 404 Not founs

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,11 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error=Not found'), 404
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
